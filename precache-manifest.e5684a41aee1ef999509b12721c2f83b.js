801d81975c64460c9c33727e826a40f317e937ab
nihelboujelben <nihelboujelben@gmail.com>
2020-08-05 13:43:21 +0100

    Updates

---
+++ precache-manifest.e5684a41aee1ef999509b12721c2f83b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ced9c19ea6fcc4416b7e97626c420a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bf453c319f06a549a0e",
+    "url": "/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "cdfb7f693f8546c8b4ff",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2bf453c319f06a549a0e",
+    "url": "/static/js/2.e1afbf22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e1afbf22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdfb7f693f8546c8b4ff",
+    "url": "/static/js/main.edeb46f0.chunk.js"
+  },
+  {
+    "revision": "8cc0ec0cbf74ddb75658",
+    "url": "/static/js/runtime-main.ff6ce385.js"
+  }
+]);
